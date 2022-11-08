d5bf8676233fe1055608183b5a2beb4e3081be13
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-08 14:19:59 -0800

    finished file layout, added wireframes, and sql seed.

---
+++ models/games.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class GameData extends Model {}
+
+GameData.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		publisher: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		platforms: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		categories: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		genres: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		game_type: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.Decimal,
+			allowNull: false,
+		},
+		game_image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		}
+	},
+	{
+		sequelize,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'gameData',
+	}
+);
+
+module.exports = Comment;
---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+const auth = (req, res, next) => {
+    if (!req.session.loggedIn) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = auth;
+  
