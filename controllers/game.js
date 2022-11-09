9bf36aa28ca77a8da2087fb58945c46086163013
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-09 15:54:15 -0800

    finished HTML routes

--- controllers/game.js
+++ controllers/game.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { GameData, Comment, User } = require('../models');
 const auth = require('../utils/auth');
 
-// Returns Game by Id
-router.get('/game/:id', auth, async (req, res) => {
+// Returns Game by user Id includes comments
+router.get('/:id', auth, async (req, res) => {
 	try {
 		const gameData = await GameData.findByPk(req.params.id, {
 			include: [
@@ -17,7 +17,7 @@ router.get('/game/:id', auth, async (req, res) => {
 			],
 		});
 		const game = gameData.get({ plain: true });
-		res.render('viewByGame', {
+		res.render('', {
 			game,
 			loggedIn: req.session.loggedIn,
 		});
@@ -28,13 +28,97 @@ router.get('/game/:id', auth, async (req, res) => {
 });
 
 // Returns Game by Category
+router.get('/category/:category', auth, async (req, res) => {
+	try {
+		const games = await GameData.findAll({
+			where: {
+				category: req.params.category,
+			},
+		});
+		const displayGames = games.map( game => game.get({ plain: true }));
+		res.render('', {
+			displayGames,
+			loggedIn: req.session.loggedIn,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
 
 // Returns Game By Publisher
+router.get('/publisher/:publisher', auth, async (req, res) => {
+	try {
+		const games = await GameData.findAll({
+			where: {
+				publisher: req.params.publisher,
+			},
+		});
+		const displayGames = games.map( game => game.get({ plain: true }));
+		res.render('', {
+			displayGames,
+			loggedIn: req.session.loggedIn,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
 
-// Returns Game Platforms 
+// Returns Game Platforms
+router.get('/publisher/:platform', auth, async (req, res) => {
+	try {
+		const games = await GameData.findAll({
+			where: {
+				platform: req.params.platform,
+			},
+		});
+		const displayGames = games.map( game => game.get({ plain: true }));
+		res.render('', {
+			displayGames,
+			loggedIn: req.session.loggedIn,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
 
 // Returns Game Genres
+router.get('/genres/:genres', auth, async (req, res) => {
+	try {
+		const games = await GameData.findAll({
+			where: {
+				genres: req.params.genres,
+			},
+		});
+		const displayGames = games.map( game => game.get({ plain: true }));
+		res.render('', {
+			displayGames,
+			loggedIn: req.session.loggedIn,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
 
-// Returns Game Price 
- 
+// Returns Game Price: asc or desc
+router.get('/price/:orderBy', auth, async (req, res) => {
+	try {
+		const games = await GameData.findAll({
+			order:[[
+				"price", req.params.orderBy 
+			]]
+		});
+		const displayGames = games.map( game => game.get({ plain: true }));
+		res.render('', {
+			displayGames,
+			loggedIn: req.session.loggedIn,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
 module.exports = router;
