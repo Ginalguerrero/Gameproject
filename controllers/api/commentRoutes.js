ffe10db01c330d4adac355cb45be74039b010bea
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-09 17:39:52 -0800

    finished commentRoutes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,9 +1,62 @@
 const router = require('express').Router();
+const { Comment } = require('../../models');
+const auth = require('../../utils/auth');
 
 // Create a new comment
+router.post('/', auth, async (req, res) => {
+	try {
+		const createComment = await Comment.create({
+			comment: req.body.comment,
+			game_id: req.body.game_id,
+			user_id: req.body.user_id
+		});
+		res.status(200).json(createComment);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
 
 // update a comment
+router.put('/:id', auth, async (req, res) => {
+	try {
+		const updatedComment = await Comment.update(
+			{
+				comment: req.body.comment,
+			},
+			{
+				where: {
+					id: req.params.id,
+				}
+			}
+		);
+		if (!updatedComment ) {
+			res.status(404).json({ message: 'No comment found with this id!' });
+			return;
+		}
+		res.status(200).json(updatedComment);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 // delete a comment 
+router.delete('/:id', auth, async (req, res) => {
+	try {
+		const deletedComment = await Comment.destroy({
+			where: {
+				id: req.params.id
+			},
+		});
+		if (!deletedComment) {
+			res.status(404).json({ message: 'No comment found with this id!' });
+			return;
+		}
+		res.status(200).json(deletedComment);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 
 module.exports = router;
