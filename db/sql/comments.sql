d5560e53d8f8b5d35aff869d21fa541d8f0abc41
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-11-10 18:05:34 -0800

    Worked with Kyle to fix the database data format, so we can parse

--- db/sql/comments.sql
+++ db/sql/comments.sql
@@ -1,3 +1,15 @@
+CREATE TABLE comment (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  comment VARCHAR(250) NOT NULL,
+  user_id INT,
+  game_id INT,
+  FOREIGN KEY (game_id)
+  REFERENCES gamedata(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (user_id)
+  REFERENCES user(id)
+  ON DELETE CASCADE
+);
 
 INSERT INTO comment (user_id, game_id, comment)
 VALUES (1,	1,	"That is the best game I have ever played in my life"),
