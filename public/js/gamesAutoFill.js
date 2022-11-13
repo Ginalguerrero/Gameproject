34b0752f055aea56178b04ac404b295c53050558
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-13 07:55:15 -0800

    got the search bar working

--- public/js/gamesAutoFill.js
+++ public/js/gamesAutoFill.js
@@ -1,7 +1,34 @@
 
 const games = [
-    "hljslkdfalksjdfkla",
-    "jsasdfasdfa", 
-    "john"
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
     ];
-$('#search').on("click", autocomplete({source: games}));
+
+
+// function getGameNames(){
+// fetch("")
+// $('#search').autocomplete({source: games});
+// }
+
+// $('#search').on("click", getGameNames);
+$('#search').autocomplete({source: games});
