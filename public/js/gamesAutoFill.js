04f5b4d139767addfadd3eb3066c9b1b62667568
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-11-14 18:55:19 -0800

    Applied some style to the Add button

--- public/js/gamesAutoFill.js
+++ public/js/gamesAutoFill.js
@@ -1,6 +1,7 @@
 const games = [];
 const gameIds = [];
 const searchResult = document.querySelector('.search');
+const searchSide = document.querySelector('#searchSide');
 
 function getGameNames() {
 	fetch('/game/name')
@@ -19,6 +20,31 @@ function getGameNames() {
 }
 
 searchResult.addEventListener('keydown', async (event) => {
+	if (event.key === 'Enter') {
+		let validate = event.target.value;
+		let onList = 0;
+		let gameId;
+		for (let i = 0; i < games.length; i++) {
+			if (validate === games[i]) {
+				onList = 1;
+				gameId = gameIds[i];
+			}
+		}
+		if (onList === 1) {
+			let id = gameId;
+				document.location.replace(`/game/${id}`);
+		} else {
+			swal({
+				title: "No game found",
+				text: "Please enter another game name.",
+				icon: "warning",
+				dangerMode: true,
+			  });
+		}
+	}
+});
+
+searchSide.addEventListener('keydown', async (event) => {
 	if (event.key === 'Enter') {
 		let validate = event.target.value;
 		let onList = 0;
